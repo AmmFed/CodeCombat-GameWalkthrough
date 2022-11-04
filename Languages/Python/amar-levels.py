344baf502d21200bf90e99fc26706c881f30d8bd
AmmFed <114851245+AmmFed@users.noreply.github.com>
2022-11-04 20:04:36 +0200

    Delete easy levels

--- Languages/Python/amar-levels.py
+++ Languages/Python/amar-levels.py
@@ -1,18 +1,1 @@
-#m1-l5 (basic syntax, object, methods, arguments/parameters, strings):
-hero.moveRight(3)
-hero.moveUp(1)
-hero.moveRight(1)
-hero.moveDown(3)
-hero.moveRight(2)
-
-#m1-l8 (basic syntax, object, methods, arguments/parameters, strings, comments):
-# Free the prisoner, defeat the guard and grab the gem.
-
-# Free Patrick from behind the "Weak Door".
-hero.attack("Weak Door")
-# Defeat the guard, named "Two".
-hero.moveRight(3)
-# Get the gem.
-hero.moveDown(3)
-hero.attack("Two")
-
+#M1:L1~L12 are all straight-forward
