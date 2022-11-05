9eb29c86df843643fec57380115c40453ac7feee
AmmFed <114851245+AmmFed@users.noreply.github.com>
2022-11-05 18:28:21 +0200

    M2:L2

--- Languages/Python/amar-levels.py
+++ Languages/Python/amar-levels.py
@@ -1,1 +1,14 @@
-#M1:L1~L16 are all straight-forward
+#M1:L1~L96 are all straight-forward
+
+#M2:L2:
+# Go to the end of the path and build a fence on the X
+# Use your moveXY(x, y) function.
+# It's the first point of the path.
+hero.moveXY(36, 59)
+# Move at the next points of the path.
+hero.moveXY(37, 12)
+# Build a fence to stop the ogre.
+hero.buildXY("fire-trap", 72, 25)
+hero.moveXY(77, 15)
+hero.moveXY(54, 17)
+hero.moveXY(73, 64)
