f53f7ce902902a6b6ce80b034daecb4c8511df1e
AmmFed <114851245+AmmFed@users.noreply.github.com>
2022-11-06 18:30:41 +0200

    Enhanced comments readability + Added M2:L11

--- Languages/Python/amar-levels.py
+++ Languages/Python/amar-levels.py
@@ -1,46 +1,64 @@
-#M1:L1~L19 are all straight-forward 
-#from "Dungeons of Kithgard" to "Kithgard Gates"
+# M1:L1~L19 are all straight-forward 
+# from "Dungeons of Kithgard" to "Kithgard Gates"
 
-#M2:L2 "Winding Trail"
-#Getting all the gems trick
-#1. Getting the first gem
+# M2:L2 "Winding Trail"
+# Getting all the gems trick
+# 1. Getting the first gem
 hero.moveXY(36, 59)
-#2. Getting the second gem
+# 2. Getting the second gem
 hero.moveXY(37, 12)
-#3. Instead of buidling a fence, build a fire-trap on the X mark
+# 3. Instead of buidling a fence, build a fire-trap on the X mark
 hero.buildXY("fire-trap", 72, 25)
-#4. Move away from the fence to the right then to the left 
-#to waste some time till the enemy reaches the fire-trap
+# 4. Move away from the fence to the right then to the left 
+# to waste some time till the enemy reaches the fire-trap
 hero.moveXY(77, 15)
 hero.moveXY(54, 17)
-#5. Get the last sweet gems!
+# 5. Get the last sweet gems!
 hero.moveXY(73, 64)
 
-#M2:L8 "Range Finder"
-#Completing the level in only 3 lines of code, 
-#just ignoring the declaration of distance and enemy variables
+# M2:L8 "Range Finder"
+# Completing the level in only 3 lines of code, 
+# just ignoring the declaration of distance and enemy variables
 hero.say(hero.distanceTo("Gort"))
 hero.say(hero.distanceTo("Smasher"))
 hero.say(hero.distanceTo("Gorgnub"))
 
-#M2:L9 "Peasant Protection"
-#Using the Boolean Operator "and" to reduce the code count
+# M2:L9 "Peasant Protection"
+# Using the Boolean Operator "and" to reduce the code count
 while True:
     enemy = hero.findNearestEnemy()
-    if enemy and hero.distanceTo(enemy) < 10: #<- Use "and" here
+    if enemy and hero.distanceTo(enemy) < 10: # <- Use "and" here
         hero.attack(enemy)
     else:
         hero.moveXY(40, 37)
 
 
-#M2:L10 "Maniac Munchkins"
-#Using multiple "and"s to reduce the code count
+# M2:L10 "Maniac Munchkins"
+# Using multiple "and"s to reduce the code count
 while True:
     hero.attack("Chest")
     enemy = hero.findNearestEnemy()
-    #Use three lines in one line
+    # Use three lines in one line
     if enemy and hero.distanceTo(enemy) < 5 and hero.isReady("cleave"): 
         hero.cleave(enemy)
-    #Use two lines in one line
+    # Use two lines in one line
     elif enemy and hero.distanceTo(enemy) < 5: 
         hero.attack(enemy)
+
+# M2:L11 "Forest Fire Dancing"
+# In this level the evilstone is bad! Avoid them, 
+# walking the other direction.
+while True:
+    evilstone = hero.findNearestItem()
+    
+    # When evilstone exists and is to the left:
+    if evilstone and evilstone.pos.x == 34:
+        hero.moveXY(46, 23)
+    
+    # When evilstone exits but is not to the left:
+    elif evilstone and evilstone.pos.x != 34:
+        hero.moveXY(34, 22)
+    
+    # When evilstone is not there
+    else:
+        hero.moveXY(40, 22)
